337dae383742e93cc11d2024480795fb89696bf6
David Thomas <pihdave@gmail.com>
2010-09-01 16:15:13 +0000

    htmlformflowsheet.  changed mdrtb links to global property with default set to 'false'

--- web/src/org/openmrs/module/htmlformflowsheet/web/html/FormListLinks.java
+++ web/src/org/openmrs/module/htmlformflowsheet/web/html/FormListLinks.java
@@ -3,6 +3,7 @@ package org.openmrs.module.htmlformflowsheet.web.html;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.openmrs.api.context.Context;
 import org.openmrs.module.web.extension.AdministrationSectionExt;
 
 public class FormListLinks extends AdministrationSectionExt {
@@ -11,7 +12,9 @@ public class FormListLinks extends AdministrationSectionExt {
     public Map<String, String> getLinks() {
         // TODO Auto-generated method stub
         Map<String, String> ret = new LinkedHashMap<String, String>();
-        ret.put("module/htmlformflowsheet/patientWidgetChart.list", "CAT IV Treatment Card Flowsheet");
+        String test = Context.getAdministrationService().getGlobalProperty("htmlformflowsheet.showMdrtbCatIVLink");
+        if (test != null && test.equals("true"))
+            ret.put("module/htmlformflowsheet/patientWidgetChart.list", "CAT IV Treatment Card Flowsheet");
         return ret;
     }
 
