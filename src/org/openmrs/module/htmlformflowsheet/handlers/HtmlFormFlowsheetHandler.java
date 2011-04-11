eecaa539e13a87d5d049a733f5d3fd27d9c11285
David Thomas <pihdave@gmail.com>
2011-04-11 16:32:04 +0000

    htmlformflowsheet.  updating classpath and lib in repository to work with openmrs-1.6, htmlformentry 1.7.2.

--- src/org/openmrs/module/htmlformflowsheet/handlers/HtmlFormFlowsheetHandler.java
+++ src/org/openmrs/module/htmlformflowsheet/handlers/HtmlFormFlowsheetHandler.java
@@ -3,6 +3,7 @@ package org.openmrs.module.htmlformflowsheet.handlers;
 import java.io.PrintWriter;
 import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
@@ -11,18 +12,38 @@ import org.openmrs.Form;
 import org.openmrs.Patient;
 import org.openmrs.module.htmlformentry.FormEntrySession;
 import org.openmrs.module.htmlformentry.FormEntryContext.Mode;
+import org.openmrs.module.htmlformentry.handler.AttributeDescriptor;
 import org.openmrs.module.htmlformentry.handler.TagHandler;
 import org.openmrs.module.htmlformflowsheet.web.utils.HtmlFormFlowsheetWebUtils;
 import org.openmrs.web.WebConstants;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
-
+/**
+ * The tag handler for rendering an htmlform within a parent htmlform.
+ * 
+ * POSSIBLE ATTRIBUTES:
+ * formId (required) -- expects the real form Id, not the htmlform ID.  Describes the form to render.
+ * sharedEncounter (optional, default = 'false') -- loads all encounters of the same EncounterType, rather than default behavior,
+ *              which is to load only Encounters mapped to the form given in formId.  Also, this will cause 
+ *              a drop-down to be appended to the display that allows you to select an encounter to append to
+ *              when performing a new entry.
+ * addAnotherButtonLabel (optional, default = 'Add Another') -- changes the text label for the 'Add Another' button.
+ * windowHeight (optional, default = 400) -- allows you to specify the height of the pop-up that you get when
+ *              you click the 'Add New' button.
+ * showHtmlForm (optional, default = 'false') If you set this to 'true', the htmlformflowsheet default table view of form observations
+ *              will be substituted with a Mode.VIEW version of the rendered htmlform itself for each encounter in the table. 
+ * readOnly (optional, default = 'false') -- if true, will render the htmlformflowsheet, but without the 
+ *              'Add Another' button, or edit links.  in htmlformentry VIEW mode, this is always false.
+ */
 public class HtmlFormFlowsheetHandler  implements TagHandler {
 
     /** The logger to use with this class */
     protected final Log log = LogFactory.getLog(getClass());
 
+    public List<AttributeDescriptor> getAttributeDescriptors(){
+    	return null;
+    }
     
     public boolean doStartTag(FormEntrySession session, PrintWriter out, Node parent, Node node) {
         Map<String, String> attributes = new HashMap<String, String>();        
@@ -32,7 +53,7 @@ public class HtmlFormFlowsheetHandler  implements TagHandler {
             attributes.put(attribute.getNodeName(), attribute.getNodeValue());
         }
         
-        
+        //formId
         String configuration = null;
         Patient patient = session.getPatient();
         try {
@@ -46,6 +67,7 @@ public class HtmlFormFlowsheetHandler  implements TagHandler {
                     + attributes);
         }
         
+        //sharedEncounter
         String showAllWithEncType = null;
         try {
             showAllWithEncType = attributes.get("sharedEncounter");
@@ -53,6 +75,7 @@ public class HtmlFormFlowsheetHandler  implements TagHandler {
             
         }
         
+        //addAnotherButtonLabel
         String addAnotherButtonLabel = (String) attributes.get("addAnotherButtonLabel");
         try {
             if (addAnotherButtonLabel != null && !addAnotherButtonLabel.equals("")){
@@ -62,14 +85,28 @@ public class HtmlFormFlowsheetHandler  implements TagHandler {
             ex.fillInStackTrace();
         }
         
+        //windowHeight
         String windowHeight = (String) attributes.get("windowHeight");
         
+        //showHtmlForm
         String showHtmlFormInstead = "false";
         String showHtmlFormInsteadStr = (String) attributes.get("showHtmlForm");
         if (showHtmlFormInsteadStr != null && showHtmlFormInsteadStr.equals("true"))
             showHtmlFormInstead = "true";
         
-            
+        //readOnly
+        //defaults
+        String readOnly = "false";
+        if (session.getContext().getMode().equals(Mode.VIEW))
+            readOnly = "true";
+        //explicitly passed in:
+        String readOnlyStr = (String) attributes.get("readOnly");
+        if (readOnlyStr != null && readOnlyStr.equals("true"))
+            readOnly = "true";
+        else if (readOnlyStr != null && readOnlyStr.equals("false"))
+            readOnly = "false";
+        
+        //setup the iframe
         if (patient != null){
             StringBuilder sb = new StringBuilder("");
             
@@ -81,10 +118,10 @@ public class HtmlFormFlowsheetHandler  implements TagHandler {
             }
             if (!session.getContext().getMode().equals(Mode.VIEW)){
                 //sb.append("  src='/openmrs/module/htmlformflowsheet/patientWidgetChart.list?fullPage=false&patientId=" + patient.getPatientId() + "&configuration=F:BOO:" + configuration + "'  ");
-                source = "'/"+WebConstants.WEBAPP_NAME+"/module/htmlformflowsheet/patientWidgetChart.list?fullPage=false&patientId=" + patient.getPatientId() + "&configuration=F:BOO:" + configuration + encounterTypeAddition + "&showHtmlForm=" + showHtmlFormInstead + "&windowHeight=" + windowHeight + "&addAnotherButtonLabel=" + addAnotherButtonLabel + "'";
+                source = "'/"+WebConstants.WEBAPP_NAME+"/module/htmlformflowsheet/patientWidgetChart.list?readOnly=" + readOnly + "&fullPage=false&patientId=" + patient.getPatientId() + "&configuration=F:BOO:" + configuration + encounterTypeAddition + "&showHtmlForm=" + showHtmlFormInstead + "&windowHeight=" + windowHeight + "&addAnotherButtonLabel=" + addAnotherButtonLabel + "'";
             } else {
                 //sb.append("  src='/openmrs/module/htmlformflowsheet/patientWidgetChart.list?readOnly=true&fullPage=false&patientId=" + patient.getPatientId() + "&configuration=F:BOO:" + configuration + "'  ");
-                source = "'/"+WebConstants.WEBAPP_NAME+"/module/htmlformflowsheet/patientWidgetChart.list?readOnly=true&fullPage=false&patientId=" + patient.getPatientId() + "&configuration=F:BOO:" + configuration + encounterTypeAddition + "&showHtmlForm=" + showHtmlFormInstead + "&windowHeight=" + windowHeight + "&addAnotherButtonLabel=" + addAnotherButtonLabel + "'";
+                source = "'/"+WebConstants.WEBAPP_NAME+"/module/htmlformflowsheet/patientWidgetChart.list?readOnly=" + readOnly + "&fullPage=false&patientId=" + patient.getPatientId() + "&configuration=F:BOO:" + configuration + encounterTypeAddition + "&showHtmlForm=" + showHtmlFormInstead + "&windowHeight=" + windowHeight + "&addAnotherButtonLabel=" + addAnotherButtonLabel + "'";
             }
             sb.append(" src='/"+WebConstants.WEBAPP_NAME+"/moduleResources/htmlformflowsheet/pleaseWait.htm'  ");
             sb.append(" width='100%' frameborder='0' scrolling='no'></iframe><br/><script>window.frames['iframeFor" + configuration + "'].innerHTML = 'please wait...'; \n function iframe"+configuration+"(){window.frames['iframeFor" + configuration + "'].location = "+source+";} \n setTimeout('iframe"+configuration+"();', 1);</script>");
